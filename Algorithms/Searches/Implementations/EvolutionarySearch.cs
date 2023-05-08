e613d71dc7b78bac4fcf63fa59d7dd1fb81ac621
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-05-08 18:16:34 +0200

    impl partial evo; todo mutations

--- Algorithms/Searches/Implementations/EvolutionarySearch.cs
+++ Algorithms/Searches/Implementations/EvolutionarySearch.cs
@@ -34,24 +34,84 @@ public class EvolutionarySearch : Search {
       };
     }
 
-
-    var best = SearchType.SteepestLocal.Search(instance, new() {
-      Initializers = { (SearchType.Random, new() { Population = population }) },
-      Variant = (int?)SteepestLocalSearch.Variant.InternalEdgeExternalVertices
-    });
-    var ts = TimeSpan.FromSeconds(timelimit);
+    var candidates = Enumerable.Range(0, 20)
+      .Select(
+        _ => SearchType.SteepestLocal.Search(instance, new() {
+          Initializers = { (SearchType.Random, new(population.Length, instance.Dimension)) },
+          Variant = (int?)SteepestLocalSearch.Variant.InternalEdgeExternalVertices
+        })
+      )
+      .Select(cycles => (cycles, distance: instance.Distance[cycles]))
+      .ToList();
     iterations = 0;
 
-    var start = DateTime.Now;
-    while (DateTime.Now - start < ts) {
-      var candidate = SearchType.WeightedRegretCycleExpansion.Search(instance, CreateConfiguration(best));
-      ++iterations;
+    const int patience = 300;
+    const int minDifference = 50;
+    var remainingPatience = patience;
 
-      if (instance.Distance[candidate] >= instance.Distance[best]) continue;
-      best = candidate;
+    var ts = TimeSpan.FromSeconds(timelimit);
+    var start = DateTime.Now;
+
+    var best = candidates.MinBy(x => x.distance);
+    var bestAt = candidates.IndexOf(best);
+    var lastBestDistance = 0;
+
+    ImmutableArray<NodeList> Mutate(ImmutableArray<NodeList> a, ImmutableArray<NodeList> b) {
+      a = a.Select(x => x.Clone()).ToImmutableArray();
+      b = b.Select(x => x.Clone()).ToImmutableArray();
+
+      foreach (var first in a) {
+        var n = first.Count;
+        if (n is 1) continue;
+
+        throw new NotImplementedException();
+      }
+
+      var f = a[0];
+      var s = b[1];
+      return SearchType.WeightedRegretCycleExpansion.Search(instance, new() {
+        Population = new[] { f, s }.ToImmutableArray(),
+        Regret = 2,
+        Weight = 0.38f,
+      });
     }
 
-    return best;
+    bool IsTooSimilarToCandidates(int distance) =>
+      candidates.Any(candidate => Math.Abs(candidate.distance - distance) < minDifference);
+
+
+    while (DateTime.Now - start < ts) {
+      iterations += 1;
+      var worst = candidates.MaxBy(candidate => candidate.distance);
+      var worstAt = candidates.IndexOf(worst);
+
+      var (first, second) = Shared.Random.Choose2(candidates);
+      var mutated = SearchType.SteepestLocal.Search(instance, new() {
+        Variant = (int?)SteepestLocalSearch.Variant.InternalEdgeExternalVertices,
+        Population = Mutate(first.cycles, second.cycles)
+      });
+
+      var candidate = (mutated, distance: instance.Distance[mutated]);
+      if (lastBestDistance > candidate.distance) {
+        lastBestDistance = candidate.distance;
+        candidates[bestAt] = candidate;
+      }
+      else if (candidate.distance < worst.distance && !IsTooSimilarToCandidates(candidate.distance)) {
+        candidates[worstAt] = candidate;
+      }
+
+      best = candidates.MinBy(x => x.distance);
+      bestAt = candidates.IndexOf(best);
+
+      if (lastBestDistance < best.distance) {
+        lastBestDistance = best.distance;
+        remainingPatience = patience;
+      }
+
+      if (--remainingPatience == 0) break;
+    }
+
+    return best.cycles;
   }
 
   private static ImmutableArray<NodeList> Constructive(Instance instance, ImmutableArray<NodeList> population,
--- Domain/Extensions/RandomExtensions.cs
+++ Domain/Extensions/RandomExtensions.cs
@@ -14,6 +14,15 @@ public static class RandomExtensions {
   public static T Choose<T>(this Random random, ImmutableArray<T> list) => list[random.Next(list.Length)];
 
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static (T, T) Choose2<T>(this Random random, IList<T> list) {
+    var a = Shared.Random.Next(list.Count);
+    var b = Shared.Random.Next(list.Count);
+    while (a == b) b = Shared.Random.Next(list.Count);
+
+    return (list[a], list[b]);
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> items) =>
     items.OrderBy(_ => Shared.Random.Next()).ToArray();
 
